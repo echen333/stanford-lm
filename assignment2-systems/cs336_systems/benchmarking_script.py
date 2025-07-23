18f8b56083db17e0950a2cc6fe8eb164a60b9cbf
Edward Chen <echen333us@gmail.com>
2025-07-23 21:09:36 +0000

    better benchmarking, mixed precision testing

--- assignment2-systems/cs336_systems/benchmarking_script.py
+++ assignment2-systems/cs336_systems/benchmarking_script.py
@@ -1,49 +1,87 @@
 import cs336_basics
 import torch
 import timeit
+import argparse
 
 from cs336_basics.utils import Transformer
 from cs336_basics.utils_train import get_batch, cross_entropy
 import numpy as np
+import torch.cuda.nvtx as nvtx
 
 
-def forward_pass(model: Transformer, x):
-    torch.cuda.synchronize()
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--d_model", type=int, default=768)
+    parser.add_argument("--d_ff", type=int, default=3072)
+    parser.add_argument("--num_layers", type=int, default=12)
+    parser.add_argument("--num_heads", type=int, default=12)
+    parser.add_argument("--data_path", type=str, default="cs336_systems/data/TinyStoriesV2-GPT4-valid.npy")
+    return parser.parse_args()
+
+
+def model_pass(model: Transformer, x, only_forward=True, y=None):
+    if not only_forward:
+        model.zero_grad(set_to_none=True)
+
+    nvtx.range_push("forward")
     out = model(x)
+    nvtx.range_pop()
+    if only_forward:
+        torch.cuda.synchronize()
+        return
+    
 
-
-def backward_pass(model: Transformer, loss):
-    torch.cuda.synchronize()
-    loss.backward(retain_graph=True)
-
-
-def benchmark(data_path, vocab_sz, d_model, n_heads, rope_theta, context_len, n_layers, device):
-    BATCH_SIZE = 4
-    NUM_WARMUPS = 5
-
-    model = Transformer(vocab_sz, d_model, n_heads, rope_theta, context_len, n_layers, device)
-    with open(data_path, "r") as f:
-        arr = np.load(data_path, mmap_mode="r")
-
-    x, y = get_batch(arr, BATCH_SIZE, context_len, device)
-
-    for _ in range(NUM_WARMUPS):
-        forward_pass(model, x)
-    timer = timeit.Timer(stmt=lambda: forward_pass(model, x))
-    print(timer.timeit(100))
-
-    out = model(x)
     out = out.flatten(0, 1)  # of shape B C V -> (B*C) V
     y = y.flatten(0, 1)
     loss = cross_entropy(out, y)
 
-    print(type(loss))
+    nvtx.range_push("backward")
+    loss.backward()
+    torch.cuda.synchronize()
+    nvtx.range_pop()
+
+def benchmark():
+    # data_path, vocab_sz, d_model, n_heads, rope_theta, context_len, n_layers, device
+    import time
+    time.sleep(2)
+
+
+    import torch
+    torch.cuda.init()
+    torch.tensor([1.0], device='cuda')  # force kernel
+    print("CUDA test done")
+
+    args = get_args()
+    args.vocab_sz = 10000
+    args.rope_theta = 10000
+    args.context_len = 128
+    args.device = "cuda"
+    print(args.num_heads, args.d_model, args.num_layers)
+    BATCH_SIZE = 4
+    NUM_WARMUPS = 5
+
+    model = Transformer(args.vocab_sz, args.d_model, args.num_heads, args.rope_theta, args.context_len, args.num_layers, args.device)
+    print("model", model.device)
+    arr = np.load(args.data_path, mmap_mode="r")
+
+    print("finished loading")
+    x, y = get_batch(arr, BATCH_SIZE, args.context_len, args.device)
+
     for _ in range(NUM_WARMUPS):
-        backward_pass(model, loss)
-    timer2 = timeit.Timer(stmt=lambda: "backward_pass(model, loss)", setup="")
-    print(timer2.timeit(100))
+        model_pass(model, x)
+    print("hihi")
+    timer = timeit.Timer(stmt=lambda: model_pass(model, x))
+    arr = np.array(timer.repeat(100, number=1))
+    print(arr)
+    print(f"statement has mean: {arr.mean()} and std: {arr.std()}")
+
+    for _ in range(NUM_WARMUPS):
+        model_pass(model, x, only_forward=False, y=y)
+    timer2 = timeit.Timer(stmt=lambda: model_pass(model, x, only_forward=False, y=y), setup="")
+    arr = np.array(timer2.repeat(100, number=1))
+    print(arr)
+    print(f"statement has mean: {arr.mean()} and std: {arr.std()}")
 
 
 if __name__ == "__main__":
-    data_path = "cs336_systems/data/TinyStoriesV2-GPT4-valid.npy"
-    benchmark(data_path, 10000, 128, 2, 10000, 32, 2, "cuda")
+    benchmark()
---
+++ assignment2-systems/cs336_systems/mixed_precision.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def mixed_precision_accumulation():
+    s = torch.tensor(0,dtype=torch.float32)
+    for i in range(1000):
+        s += torch.tensor(0.01,dtype=torch.float32)
+    print(s)
+
+    s = torch.tensor(0,dtype=torch.float16)
+    for i in range(1000):
+        s += torch.tensor(0.01,dtype=torch.float16)
+    print(s)
+
+    s = torch.tensor(0,dtype=torch.float32)
+    for i in range(1000):
+        s += torch.tensor(0.01,dtype=torch.float16)
+    print(s)
+
+    s = torch.tensor(0,dtype=torch.float32)
+    for i in range(1000):
+        x = torch.tensor(0.01,dtype=torch.float16)
+        s += x.type(torch.float32)
+    print(s)
+
+class ToyModel(nn.Module):
+    def __init__(self, in_features: int, out_features: int):
+            super().__init__()
+            self.fc1 = nn.Linear(in_features, 10, bias=False)
+            self.ln = nn.LayerNorm(10)
+            self.fc2 = nn.Linear(10, out_features, bias=False)
+            self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        print("after first", x.dtype)
+        x = self.ln(x)
+        print("after sec", x.dtype)
+        x = self.fc2(x)
+        print("after third", x.dtype)
+        return x
+
+def benchmarking_mixed_precision():
+    device = "cuda"
+    model = ToyModel(100, 5).to(device)
+    with torch.amp.autocast("cuda", dtype=torch.float16):
+        input = torch.rand(100).to(device)
+        print(input, input.dtype)
+        out = model(input).to("cpu")
+        y = torch.tensor(2)
+        loss = F.cross_entropy(out, y)
+        print(out, out.dtype)
+        print(f"loss, {loss.item()} and dtype: {loss.dtype}")
+        
+if __name__ == "__main__":
+    # mixed_precision_accumulation()
+    benchmarking_mixed_precision()
